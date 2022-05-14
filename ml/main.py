b0793344c38015d28832c8fdf18a209ebdad3d8a
Nikolai Herrmann <nikolaiherrmann@gmail.com>
2022-05-14 13:43:51 +0200

    started graphs

--- ml/main.py
+++ ml/main.py
@@ -1,9 +1,8 @@
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, learning_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 from sklearn import metrics
 from matplotlib import pyplot as plt
 import statsmodels.api as sm
@@ -12,13 +11,17 @@ import pandas as pd
 import glob
 import os
 import seaborn as sns
+import numpy as np
 
 
-DATA_PATH = "../data/all_tracking.csv"
-DATA_SPLIT = 0.01
+DATA_PATH = os.path.join("..", "data", "all_tracking.csv")
+DATA_SPLIT = 0.3
+FOLDS = 5
+KNN_N_NEIGHBORS = 3
 
 
 def save(model, model_name, dir="model", ext=".pkl"):
+    model_name = model_name.replace(" ", "_").lower()
     path = os.path.join("..", dir)
     if not os.path.isdir(path):
         os.mkdir(path)
@@ -36,6 +39,45 @@ def concat_csv(name="all_tracking"):
     df.to_csv(os.path.join(dir, name + ".csv"), index=False)
 
 
+def plot_learning_curve(model, model_name, X, y, folds):
+    """
+    Adapted from https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html
+    """
+    train_sizes, train_scores, test_scores = learning_curve(
+        model, X, y, cv=folds, n_jobs=None, return_times=False, scoring='f1_weighted'
+    )
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+
+    _, axes = plt.subplots(figsize=(8, 5))
+    axes.grid()
+    axes.fill_between(
+        train_sizes,
+        train_scores_mean - train_scores_std,
+        train_scores_mean + train_scores_std,
+        alpha=0.1,
+        color="orange",
+    )
+    axes.fill_between(
+        train_sizes,
+        test_scores_mean - test_scores_std,
+        test_scores_mean + test_scores_std,
+        alpha=0.1,
+        color="purple",
+    )
+    axes.plot(train_sizes, train_scores_mean, "o-", color="orange", label="training")
+    axes.plot(train_sizes, test_scores_mean, "o-", color="purple", label="cross-validation (" + str(folds) + " folds)")
+    axes.set_xlabel("Number of Observations", fontsize=12)
+    axes.set_ylabel("F1 Score", fontsize=12)
+    axes.legend(loc="best")
+
+    plt.title(model_name + " Learning Curve", fontsize=12)
+    plt.savefig("l_curve_" + model_name + ".pdf")
+
+
 def fit(model, model_name, x_train, y_train, x_test, y_test):
     model.fit(x_train, y_train)
 
@@ -43,14 +85,14 @@ def fit(model, model_name, x_train, y_train, x_test, y_test):
 
     y_pred = model.predict(x_test)
 
-    print(model_name)
-    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-    print("Recall:", metrics.recall_score(y_test, y_pred))
-    print("F1 Score:", metrics.f1_score(y_test, y_pred), "\n")
-    print("Confusion Matrix: \n", metrics.confusion_matrix(y_test, y_pred), "\n")
-    print("Classification Report: \n", metrics.classification_report(y_test, y_pred))
+    acc = metrics.accuracy_score(y_test, y_pred)
+    f1_score = metrics.f1_score(y_test, y_pred)
+    print(model_name, "\t", round(acc, 3), "\t\t", round(f1_score, 3))
+    # print("Recall:", metrics.recall_score(y_test, y_pred))
+    # print("Confusion Matrix: \n", metrics.confusion_matrix(y_test, y_pred), "\n")
+    # print("Classification Report: \n", metrics.classification_report(y_test, y_pred))
 
-    if model_name == "reg":
+    if model_name == "Logistic Regression":
         X2 = sm.add_constant(x_train)
         est = sm.OLS(y_train, X2)
         est2 = est.fit()
@@ -58,30 +100,39 @@ def fit(model, model_name, x_train, y_train, x_test, y_test):
 
 
 def coor_plot(df):
-    fig, ax = plt.subplots(figsize=[10, 50])
-    sns.heatmap(df.corr(), cmap = 'coolwarm', square=True, ax=ax, 
-                annot=True, linewidths=2, vmax=1, linecolor='white', 
+    _, ax = plt.subplots(figsize=(11, 8))
+    sns.heatmap(df.corr(), cmap='coolwarm', square=True, ax=ax,
+                annot=True, linewidths=2, vmax=1, linecolor='white',
                 annot_kws={'fontsize': 13})
     plt.title('Feature Correlation', y=1.05, size=16)
-    plt.show()
+    plt.savefig("feat_coor.pdf")
 
 
-def train(y_col="class"):
-    df = pd.read_csv(DATA_PATH)
+def get_data(path, y_col="class"):
+    df = pd.read_csv(path)
+    #df = np.array_split(df, 12)[0] # for quick testing
+    X = df.drop(y_col, axis=1)
+    y = df[y_col]
+    return X, y
 
-    data = df.drop(y_col, axis=1)
 
-    coor_plot(data)
+def train(X, y, split, seed=42):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split, shuffle=True, random_state=seed)
 
-    target = df[y_col]
-    x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=DATA_SPLIT)
+    models = {"Naive Bayes": GaussianNB(),
+              "KNN": KNeighborsClassifier(n_neighbors=KNN_N_NEIGHBORS),
+              "Logistic Regression": LogisticRegression(max_iter=1000)}
 
-    fit(GaussianNB(), "bayes", x_train, y_train, x_test, y_test)
-    fit(KNeighborsClassifier(n_neighbors=3), "knn", x_train, y_train, x_test, y_test)
-    #fit(SVC(kernel="linear"), "svm", x_train, y_train, x_test, y_test)
-    fit(LogisticRegression(max_iter=1000), "reg", x_train, y_train, x_test, y_test)
+    print("\t Accuracy \t F1-Score")
+    for name, model in models.items():
+        fit(model, name, X_train, y_train, X_test, y_test)
+        plot_learning_curve(model, name, X, y, FOLDS)
 
 
 if __name__ == "__main__":
-    train()
-    #concat_csv()
+    X, y = get_data(DATA_PATH)
+
+    #coor_plot(X)
+
+    train(X, y, DATA_SPLIT)
+    # concat_csv()
